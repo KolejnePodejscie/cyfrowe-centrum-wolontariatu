64932e9cfaf62c97653f22600d4538dc75c3b13b
Eg-Nickname <djkubax@op.pl>
2025-10-04 20:48:08 +0200

    =Default uuid generation on db

--- backend/migrations/00007_task_assignment/index.sql
+++ backend/migrations/00007_task_assignment/index.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS taskAssignment(
-    taskId UUID PRIMARY KEY NOT NULL,
+    taskId UUID NOT NULL,
     FOREIGN KEY (taskId) REFERENCES eventTasks (id) ON DELETE CASCADE,
     volounteerId UUID NOT NULL,
     FOREIGN KEY (volounteerId) REFERENCES users (id) ON DELETE CASCADE,
-    hoursWorked INT NOT NULL DEFAULT 0
+    hoursWorked INT NOT NULL DEFAULT 0,
+    PRIMARY KEY(taskId, volounteerId)
 );
