57fea7fcd6d06fd7235a0a3ed67934eee2f73780
Eg-Nickname <djkubax@op.pl>
2025-10-04 17:30:42 +0200

    fix: changed text field to deafault "" instead of Null

--- backend/migrations/00001_user_table/index.sql
+++ backend/migrations/00001_user_table/index.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY UNIQUE NOT NULL,
     displayname VARCHAR(48) NOT NULL,
-    description TEXT, 
+    description TEXT NOT NULL DEFAULT "", 
     email CITEXT NOT NULL,
     isAdult BOOLEAN NOT NULL DEFAULT false,
     proileImage TEXT,
--- backend/migrations/00002_organisation_table/index.sql
+++ backend/migrations/00002_organisation_table/index.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS organisation (
     id UUID PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL DEFAULT "",
     profileImage TEXT,
     backgroundImage TEXT,
 );
