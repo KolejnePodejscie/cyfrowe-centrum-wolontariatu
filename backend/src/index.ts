c89f86cc14fab8102c1eb8e19f5daf65119f3669
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-10-04 13:21:49 +0200

    fix: expand .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { logger } from "./logger.js";
 import { app } from "./server.js";
 import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 
-dotenv.config();
+const env = dotenv.config();
+dotenvExpand.expand(env);
+console.log(process.env.DB_URL);
 
 app.listen(process.env.PORT, () => {
     logger.info(`Running on ${process.env.PORT}`);
